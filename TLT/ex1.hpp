9764fe6ade2516ff4266c1cd5dde958757b4e3c0
Loctryl <letalle.thibault@gmail.com>
2022-01-19 16:10:39 +0100

    Oui

---
+++ TLT/ex1.hpp
@@ -0,0 +1,33 @@
+using namespace std;
+
+class MainSDLWindow {
+  private:
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+
+  public:
+    MainSDLWindow() {
+      window = NULL;
+      renderer = NULL;
+    }
+
+    ~MainSDLWindow(){
+      SDL_DestroyRenderer(renderer);
+      SDL_DestroyWindow(window);
+      SDL_Quit();
+    }
+
+    int Init(const char* name, int width, int height){
+
+      window = SDL_CreateWindow(name, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_MINIMIZED);
+
+      renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+      if(!renderer || !window){//initialisation de la SDL
+        cout << "Erreur d'initialisation de la SDL : "<< SDL_GetError() << endl;//on affiche l'erreur
+        return EXIT_FAILURE;//on sort du programme pour éviter de plus gros problèmes
+      } else {return EXIT_SUCCESS; }
+
+      SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+    }
+};
--- TLT/snaketlt.c++
+++ TLT/snaketlt.c++
@@ -1,28 +1,15 @@
 #include <stdio.h>
 #include <iostream>
+#include <SDL2/SDL.h>
+#include <stdlib.h>
+
+#include "ex1.hpp"
+
 using namespace std;
 
-class MClasse {
-  private:
-    int a;
-
-  public:
-    MClasse() {     // <- Constructeur, ici initialise le membre =a=
-      this->a = 0;
-    }
-
-    ~MClasse() {     // <- Destructeur, ici ne fait rien...
-    }
-
-    int add(int b) {  // méthode publique car définie dans le bloc 'public:'
-      return this-> a + b; // "this" est un pointeur sur l'objet lui-même, "->" permet d'accéder à ses membres
-    }          // membre privé car défini dans le bloc 'private:'
-}; // Ne pas oublier le ';' final !
 
 int main(void){
-  MClasse ABC ;
-  ABC.add(5);
-  cout << "OUI\n";
-
-  return 0;
+  MainSDLWindow main_window;
+  main_window.Init("snake", 500, 500);
+  SDL_Delay(3000);
 }
